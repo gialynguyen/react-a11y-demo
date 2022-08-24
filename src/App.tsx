6d4e0d51745b236fcf20a3562950b10288fccd3c
Steven Nguyen <steven@boostcommerce.net>
2022-08-24 19:10:39 +0700

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <input id="promo" />
+      <label>Receive promotional offers?</label>
+    </div>
+  );
+}
+
+export default App;
