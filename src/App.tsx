5e9b14994e2fbb7122b38e0fb518f4f1c5f27314
Steven Nguyen <steven@boostcommerce.net>
2022-08-24 20:43:04 +0700

    feat(eslint-a11y): lint script

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <label htmlFor="promo">Receive promotional offers? </label>
       <input id="promo" />
-      <label htmlFor="promo">Receive promotional offers?</label>
     </div>
   );
 }
