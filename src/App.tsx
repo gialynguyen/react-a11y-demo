dbe152e508d1f26f562c10bde6658335a67cf29b
Steven Nguyen <steven@boostcommerce.net>
2022-08-24 20:27:14 +0700

    feat(axe): setup axe-react, axe cli

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <h1>A11y</h1>
+      <label htmlFor="promo">Receive promotional offers? </label>
       <input id="promo" />
-      <label>Receive promotional offers?</label>
     </div>
   );
 }
